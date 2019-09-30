1b15f3298e9d412b69fa06f3435b0c6dcdbd38e9
Christophe Delaere <christophe.delaere@cern.ch>
2019-09-30 15:25:46 +0200

    Adapt the config to first run a track refit
    In order to gain access to track local angle at each point, one needs
    the trajectory object, and therefore a track refit. Included in this
    config is:
    - beamspot reco
    - track filter
    - track refit
    The config was inspired by the example at
    https://github.com/cms-sw/cmssw/blob/master/RecoTracker/TrackProducer/test/TrackRefit.py
    (with the addition of the BS reco and adaptation of track rsources

--- UserCode/CPEanalyzer/test/CPEanconfig.py
+++ UserCode/CPEanalyzer/test/CPEanconfig.py
@@ -2,7 +2,23 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("CPEana")
 
-# initialize MessageLogger and output report
+### Standard Configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+
+### global tag
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data_GRun', '')
+# process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc_GRun', '')
+
+
+### initialize MessageLogger and output report
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
 process.MessageLogger.categories.append('CPEana')
@@ -11,20 +27,40 @@ process.MessageLogger.cerr.INFO = cms.untracked.PSet(
         )
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
+### Events and data source
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
   '/store/express/Run2018D/StreamExpress/ALCARECO/SiStripCalMinBias-Express-v1/000/321/305/00001/FE4CA4CC-77A0-E811-A85E-FA163E0CB594.root'
                                                                            )
                            )
 
-process.demo = cms.EDAnalyzer('CPEanalyzer',
+### Track refitter specific stuff
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
+import RecoTracker.TrackProducer.TrackRefitter_cfi
+import CommonTools.RecoAlgos.recoTrackRefSelector_cfi
+process.mytkselector = CommonTools.RecoAlgos.recoTrackRefSelector_cfi.recoTrackRefSelector.clone()
+process.mytkselector.src = 'ALCARECOSiStripCalMinBias'
+process.mytkselector.quality = ['highPurity']
+process.mytkselector.min3DLayer = 2
+process.mytkselector.ptMin = 0.5
+process.mytkselector.tip = 1.0
+process.myRefittedTracks = RecoTracker.TrackProducer.TrackRefitter_cfi.TrackRefitter.clone()
+process.myRefittedTracks.src= 'mytkselector'
+process.myRefittedTracks.NavigationSchool = ''
+process.myRefittedTracks.Fitter = 'FlexibleKFFittingSmoother'
+
+### Analyzer
+process.CPEanalysis = cms.EDAnalyzer('CPEanalyzer',
                                          minTracks=cms.untracked.uint32(0),
-                                         tracks = cms.untracked.InputTag("ALCARECOSiStripCalMinBias","")
+                                         tracks = cms.untracked.InputTag("ALCARECOSiStripCalMinBias",""),
+                                         trajectories = cms.untracked.InputTag('myRefittedTracks')
                              )
 
+### TFileService: output histogram or ntuple
 process.TFileService = cms.Service("TFileService",
                                        fileName = cms.string('histodemo.root')
                                   )
 
-process.p = cms.Path(process.demo)
+### Finally, put together the sequence
+process.p = cms.Path(process.offlineBeamSpot*process.mytkselector+process.myRefittedTracks+process.CPEanalysis)
+
